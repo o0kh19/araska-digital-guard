ad1af08cea0d8bd6538b12e53a9b1a4f505eac4a
o0moh0o <193801993+o0moh0o@users.noreply.github.com>
2026-05-06 20:33:20 +0100

    Fix GitHub Pages routing with basename and router links

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/araska-digital-guard">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/services" element={<Services />} />

b58bbda2890587d25883c9afe22e875a3268455a
o0moh0o <193801993+o0moh0o@users.noreply.github.com>
2026-05-06 20:29:43 +0100

    Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/araska-digital-guard",
   server: {
     host: "::",
     port: 8080,
